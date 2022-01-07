59bd9cff706d26738e665023986c0334ef59605c
Okikiola Omotosho <omotoshookiki@gmail.com>
2022-01-07 02:34:44 +0100

    Base files created

---
+++ src/Home/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home">
+      {/* Navbar */}
+      {/* Landing page */}
+      {/* Why page with a video */}
+      {/* Services */}
+      {/* Portfolio */}
+      {/* Contact us */}
+      {/* Footer */}
+    </div>
+  );
+}
+
+export default Home;
